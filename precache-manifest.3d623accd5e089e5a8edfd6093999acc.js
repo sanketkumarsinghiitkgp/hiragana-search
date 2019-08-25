ccfb87ed6ac24629aff70d6a508bf242537ceb9a
Sanket Kumar Singh <sanketkumarsingh@iitkgp.ac.in>
2019-08-25 18:53:57 +0530

    Updates

---
+++ precache-manifest.3d623accd5e089e5a8edfd6093999acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc04244a789fbb76a879451b9dd9d7c8",
+    "url": "/hiragana-search/index.html"
+  },
+  {
+    "revision": "6012c82f9c1a8adb69e7",
+    "url": "/hiragana-search/static/css/main.b946e59c.chunk.css"
+  },
+  {
+    "revision": "4a986f17346f2d83d095",
+    "url": "/hiragana-search/static/js/2.c1795bd7.chunk.js"
+  },
+  {
+    "revision": "6012c82f9c1a8adb69e7",
+    "url": "/hiragana-search/static/js/main.aa21a825.chunk.js"
+  },
+  {
+    "revision": "6f0142c6c6951dd9e6db",
+    "url": "/hiragana-search/static/js/runtime~main.04b798a8.js"
+  },
+  {
+    "revision": "89387884ce8dd5b89bdd67ee900ed954",
+    "url": "/hiragana-search/static/media/loading.89387884.svg"
+  }
+]);
